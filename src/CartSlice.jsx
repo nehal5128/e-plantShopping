055ffcefaaad81d05a5d1f0c0809ac2a302e0ede
nehaldave <nehal.pancholi@gmail.com>
2025-02-12 07:20:39 -0500

    Updated files

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,30 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+        const existingCartItem = state.cartItems.find(item => item.id === action.payload.id);
+        if(existingCartItem) {
+            existingCartItem.quantity++;
+        } else {
+            state.items.push({ name, image, cost, quantity: 1 });
+        }
     },
     removeItem: (state, action) => {
+        const existingCartItem = state.cartItems.find(item => item.id === action.payload.id);
+            if(existingCartItem.quantity === 1) {
+                state.cartItems = state.cartItems.filter(item => item.id !== action.payload.id);
+            } else {
+                existingCartItem.quantity--;
+            }
     },
     updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload;
 
-    
+        const existingCartItem = state.cartItems.find(item => item.id === action.payload.id);
+        if(existingCartItem) {
+            
+            existingCartItem.quantity = quantity;
+        }
     },
   },
 });
